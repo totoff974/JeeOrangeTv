03b67af8a04ee01a1d19df47c96f2b2946e35b4c
totoff974 <totoff974@gmail.com>
2016-12-18 20:27:33 +0400

    nettoyage des fichiers

--- desktop/php/JeeOrangeTv.php
+++ desktop/php/JeeOrangeTv.php
@@ -114,7 +114,6 @@ foreach ($eqLogics as $eqLogic) {
 					<div class="col-sm-2">
 						<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="box_ip" placeholder="{{Adresse IP}}"/>
 					</div>
-					<input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer Widget}}" data-l1key="configuration" data-l2key="widget" checked />
 				</div>
 
 			</fieldset> 
