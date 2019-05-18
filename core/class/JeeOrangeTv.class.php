83649f9270dc5d3c38c915b33484f58eb921f5a5
totoff974 <totoff974@gmail.com>
2019-05-18 16:53:53 +0400

    lien images

--- core/class/JeeOrangeTv.class.php
+++ core/class/JeeOrangeTv.class.php
@@ -420,7 +420,7 @@ class JeeOrangeTv extends eqLogic {
             if ($inf->getName() == 'Etat Decodeur') {
             $etat_decodeur = $inf->getConfiguration('etat_decodeur');
                 if ($etat_decodeur == 1){
-                    $replace['#etat_decodeur#'] = '<img id="ON" src="plugins/JeeOrangeTv/doc/images/dashWidget/on_' . $theme . '.png" style="position:absolute;top:0;left:0px;">';
+                    $replace['#etat_decodeur#'] = '<img id="ON" src="./images/Widget/on_' . $theme . '.png" style="position:absolute;top:0;left:0px;">';
                 } else {
                     $replace['#etat_decodeur#'] = '';
                 }
@@ -429,11 +429,11 @@ class JeeOrangeTv extends eqLogic {
             if ($inf->getName() == 'Chaine Actuelle') {
             if ($inf->getConfiguration('chaine_actuelle')=='home' OR $inf->getConfiguration('chaine_actuelle')=='vod') {
                 $nom_chaine_act = $inf->getConfiguration('chaine_actuelle');
-                $replace['#cmd_chaine_act#'] = '<img id="ON" src="plugins/JeeOrangeTv/doc/images/mosaique/' . $nom_chaine_act . '.png" style="position:absolute;top:63px;left:116px;">';
+                $replace['#cmd_chaine_act#'] = '<img id="ON" src="./images/Mosaique/' . $nom_chaine_act . '.png" style="position:absolute;top:63px;left:116px;">';
             }
             else {
                 $nom_chaine_act = $this->lecture_json('logo', 'logo', $localisation, $inf->getConfiguration('chaine_actuelle'));
-                $replace['#cmd_chaine_act#'] = '<img id="ON" src="plugins/JeeOrangeTv/doc/images/mosaique/' . $nom_chaine_act . '.png" style="position:absolute;top:63px;left:116px;">';
+                $replace['#cmd_chaine_act#'] = '<img id="ON" src="./images/Mosaique/' . $nom_chaine_act . '.png" style="position:absolute;top:63px;left:116px;">';
             }
             }
         }
