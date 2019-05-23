0eb37134a45d2d873438e82616ec1aadae14caa4
totoff974 <totoff974@gmail.com>
2019-05-23 23:50:37 +0400

    Update JeeOrangeTv.js

--- desktop/js/JeeOrangeTv.js
+++ desktop/js/JeeOrangeTv.js
@@ -62,6 +62,7 @@ $('#bt_autoChaine').on('click', function () {
                                 // if ($("select[id='templateChaine']").val() == "0"){
                                 bootbox.confirm('{{Etes-vous sûr de vouloir récréer toutes les commandes ? Cela va supprimer les commandes existantes}}', function (result) {
                                     if (result) {
+                                        $('#div_alert').showAlert({message: '{{Opération réalisée avec succès : }}' + $("select[id='templateChaine']").val(), level: 'success'});
                                         // $.ajax({
                                             // type: "POST",
                                             // url: "plugins/JeeOrangeTv/core/ajax/JeeOrangeTv.ajax.php",
